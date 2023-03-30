87245c4242d1a1f19d4ea1a7acd67883ff8ef6d2
Ezra Dongijena <ezrakudzi@gmail.com>
2023-03-30 12:10:38 -0700

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,38 +1,158 @@
+0x12. C - Singly linked lists
+0x12. C - Singly linked lists
+
+
+
+
+README.md
+
+
+Project:
+
+0x12. C - Singly linked lists
+
+
+
+==========================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+lists.h
+
+
+#ifndef LISTS_H
+
+#define LISTS_H
+
+
+/**
+
+ * struct list_s - singly linked list
+
+ * @str: string - (malloc'ed string)
+
+ * @len: length of the string
+
+ * @next: points to the next node
+
+ *
+
+ * Description: singly linked list node structure
+
+ * for Holberton project
+
+ */
+
+typedef struct list_s
+
+{
+
+        char *str;
+
+        unsigned int len;
+
+        struct list_s *next;
+
+} list_t;
+
+
+size_t print_list(const list_t *h);
+
+size_t list_len(const list_t *h);
+
+list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+0-print_list.c
+
+
+#include <stdio.h>
+
 #include "lists.h"
 
 
 /**
 
- * print_listint - prints all the elements of a linked list
+ * print_list - prints all the elements of a linked list
 
- * @h: linked list of type listint_t to print
+ * @h: pointer to the list_t list to print
 
  *
 
- * Return: number of nodes
+ * Return: the number of nodes printed
 
  */
 
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 
 {
 
- size_t num = 0;
+ size_t s = 0;
 
 
  while (h)
 
  {
 
- printf("%d\n", h->n);
+ if (!h->str)
 
- num++;
+ printf("[0] (nil)\n");
+
+ else
+
+ printf("[%u] %s\n", h->len, h->str);
 
  h = h->next;
+
+ s++;
 
  }
 
 
- return (num);
+ return (s);
 
 }
